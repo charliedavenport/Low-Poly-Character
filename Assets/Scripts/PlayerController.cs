c811247e45ed1d897301b43f0411d9a44d5d78c2
Charles Davenport <cdavenport090@gmail.com>
2018-07-13 14:49:16 -0400

    locomotion: added sprint, blending between walk and sprint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed = 5f;
-    public bool sprinting { get; private set; }
+    public float BaseMoveSpeed = 1f;
+    public float SprintMoveSpeed = 2f;
+    public bool sprinting;
 
     private Animator anim;
     private Vector2 inputAxes;
+    public float moveSpeed;
+    private float speedPercent;
+    private const float speedPercent_Max = 2f;
 
     private void Awake()
     {
+        moveSpeed = BaseMoveSpeed;
         anim = GetComponent<Animator>();
         inputAxes = Vector2.zero;
         sprinting = false;
@@ -21,7 +26,21 @@ public class PlayerController : MonoBehaviour {
     {
         inputAxes.x = Input.GetAxis("Horizontal");
         inputAxes.y = Input.GetAxis("Vertical");
-        anim.SetFloat("SpeedPercent",  Mathf.Clamp01(inputAxes.magnitude));
+        sprinting = Input.GetKey(KeyCode.LeftShift);
+        if (!sprinting)
+        {
+            speedPercent = Mathf.Lerp(speedPercent, Mathf.Clamp01(inputAxes.magnitude), 0.1f);
+            moveSpeed = Mathf.Lerp(moveSpeed, BaseMoveSpeed, 0.3f);
+        }
+        if (sprinting)
+        {
+            //speedPercent = Mathf.Clamp01(inputAxes.magnitude);
+            speedPercent = Mathf.Lerp(speedPercent, 2 , 0.1f);
+            moveSpeed = Mathf.Lerp(moveSpeed, SprintMoveSpeed, 0.3f);
+        }
+        anim.SetFloat("SpeedPercent", speedPercent);
+
+
     }
 
     private void FixedUpdate()
@@ -33,6 +52,20 @@ public class PlayerController : MonoBehaviour {
             Debug.DrawRay(transform.position, lookAt);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookAt, Vector3.up), 0.25f);
             Debug.DrawRay(transform.position + Vector3.up, transform.forward);
+        }
+    }
+
+    IEnumerator doSprint()
+    {
+        for (float t=0; t<1; t += 0.1f)
+        {
+            speedPercent = Mathf.SmoothStep(speedPercent, speedPercent_Max, t);
+            anim.SetFloat("SpeedPercent", speedPercent);
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                break;
+            }
+            yield return new WaitForFixedUpdate();
         }
     }
 
